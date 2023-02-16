992fc2f957591b8e7661474f095b09a62d449aff
Wilson <wefigueroa0@misena.edu.co>
2023-02-16 10:29:24 -0500

    doshoras  4

--- routes/storage.js
+++ routes/storage.js
@@ -1,34 +1,12 @@
-const multer = require ("multer")
 const express =require("express");
 const router= express.Router();
-//TOD http:// localhost:3000/storage
+const uploadMiddleware = require("../utils/hadleStorage") 
+const { createItem } = require("../controllers/storage")
+//TODO http:// localhost:3000/storage
 
 
 
-/**
- * 
- */
 
-const storage = multer. diskStorage({
-    destination:function(req, file, cb){
-     const pathStorage = `${__dirname}/../storage`;
-     cb(null, pathStorage);
-    },
-    filename:function(req, file, cb) {
-        const ext  = file.originalname.split(".").pop();
-        const filename = `file-${Date.now()}.${ext}`;
-        cb(null, filename)
-}
-})
+router.post("/", uploadMiddleware.single("myfile"), createItem);
 
-const uploadMiddleware = multer ({storage})
-
-/**
- * 
- */
-
-router.post("/", uploadMiddleware.single("myfile"), (req, res ) =>{
-    res.send ({a:1})
-})
-
-module.exports=router
+module.exports= router;
